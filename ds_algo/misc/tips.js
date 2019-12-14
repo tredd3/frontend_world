6b5a41cf6911cad22459e2e36343edd40639e75e
tarakreddy <lopinti.reddy@ril.com>
2019-12-15 00:07:00 +0530

    changes

--- ds_algo/misc/tips.js
+++ ds_algo/misc/tips.js
@@ -17,3 +17,115 @@ for (var i = 0, length = array.length; i < length; i++) {
 var array = [1, 2, 3, 4, 5, 6];
 console.log(array.slice(-1)); // [6]
 console.log(array.slice(-2)); // [5,6]
+
+function flatten(arr) {
+    const result = []
+
+    arr.forEach((i) => {
+        if (Array.isArray(i)) {
+            result.push(...flatten(i))
+        } else {
+            result.push(i)
+        }
+    })
+
+    return result
+}
+
+// Usage
+const problem = [1, 2, 3, [4, 5, [6, 7], 8, 9]]
+
+flatten(problem) // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+//Counting instances of values in an object
+var names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+
+var countedNames = names.reduce(function (allNames, name) {
+    if (name in allNames) {
+        allNames[name]++;
+    }
+    else {
+        allNames[name] = 1;
+    }
+    return allNames;
+}, {});
+// countedNames is:
+// { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
+
+//Grouping objects by a property
+var people = [
+    { name: 'Alice', age: 21 },
+    { name: 'Max', age: 20 },
+    { name: 'Jane', age: 20 }
+];
+
+function groupBy(objectArray, property) {
+    return objectArray.reduce(function (acc, obj) {
+        var key = obj[property];
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(obj);
+        return acc;
+    }, {});
+}
+
+var groupedPeople = groupBy(people, 'age');
+// groupedPeople is:
+// { 
+//   20: [
+//     { name: 'Max', age: 20 }, 
+//     { name: 'Jane', age: 20 }
+//   ], 
+//   21: [{ name: 'Alice', age: 21 }] 
+// }
+
+
+//Remove duplicate items in array
+var myArray = ['a', 'b', 'a', 'b', 'c', 'e', 'e', 'c', 'd', 'd', 'd', 'd'];
+var myOrderedArray = myArray.reduce(function (accumulator, currentValue) {
+    if (accumulator.indexOf(currentValue) === -1) {
+        accumulator.push(currentValue);
+    }
+    return accumulator
+}, [])
+console.log(myOrderedArray);
+
+
+//Running Promises in Sequence
+function runPromiseInSequence(arr, input) {
+    return arr.reduce(
+        (promiseChain, currentFunction) => promiseChain.then(currentFunction),
+        Promise.resolve(input)
+    );
+}
+
+// promise function 1
+function p1(a) {
+    return new Promise((resolve, reject) => {
+        resolve(a * 5);
+    });
+}
+
+// promise function 2
+function p2(a) {
+    return new Promise((resolve, reject) => {
+        resolve(a * 2);
+    });
+}
+
+// function 3  - will be wrapped in a resolved promise by .then()
+function f3(a) {
+    return a * 3;
+}
+
+// promise function 4
+function p4(a) {
+    return new Promise((resolve, reject) => {
+        resolve(a * 4);
+    });
+}
+
+const promiseArr = [p1, p2, f3, p4];
+runPromiseInSequence(promiseArr, 10).then(console.log);   // 1200
