9362dc844bf8175b3daa11a67bf1d3d6de562397
tarakreddy <lopinti.reddy@ril.com>
2019-12-22 14:16:40 +0530

    queue

--- ds_algo/data_structures/stack/stackProblems.js
+++ ds_algo/data_structures/stack/stackProblems.js
@@ -1,1 +1,29 @@
 //check for balanced parentheses
+function checkBalancedParenthesis(input) {
+    var stack = [];
+    function charCheck(char) {
+        switch (char) {
+            case ')':
+                return stack[stack.length - 1] === '(';
+            case '}':
+                return stack[stack.length - 1] === '{';
+            case ']':
+                return stack[stack.length - 1] === '[';
+        }
+    }
+    for (var i = 0; i < input.length; i++) {
+        var char = input.charAt(i);
+        if (char === '(' || char === '{' || char === '[') {
+            stack.push(char);
+        } else if (char === ')' || char === '}' || char === ']') {
+            if (!stack.length || !charCheck(char)) {
+                return false;
+            } else {
+                stack.pop()
+            }
+        }
+    }
+    return !stack.length
+}
+
+//
