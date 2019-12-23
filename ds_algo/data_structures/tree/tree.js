fd52f3f0a216c3bcb9c7bcc024454458a2cc7056
tarakreddy <lopinti.reddy@ril.com>
2019-12-23 10:59:53 +0530

    tree recursive

--- ds_algo/data_structures/tree/tree.js
+++ ds_algo/data_structures/tree/tree.js
@@ -12,33 +12,46 @@ class BST {
     constructor() {
         this.root = null;
     }
-    add(data) {
-        const node = this.root;
-        if (node === null) {
-            this.root = new Node(data);
-            return;
+    // add(data) {
+    //     const node = this.root;
+    //     if (node === null) {
+    //         this.root = new Node(data);
+    //         return;
+    //     } else {
+    //         const searchTree = function (node) {
+    //             if (data < node.data) {
+    //                 if (node.left === null) {
+    //                     node.left = new Node(data);
+    //                     return;
+    //                 } else if (node.left !== null) {
+    //                     return searchTree(node.left);
+    //                 }
+    //             } else if (data > node.data) {
+    //                 if (node.right === null) {
+    //                     node.right = new Node(data);
+    //                     return;
+    //                 } else if (node.right !== null) {
+    //                     return searchTree(node.right);
+    //                 }
+    //             } else {
+    //                 return null;
+    //             }
+    //         };
+    //         return searchTree(node);
+    //     }
+    // }
+    add(data, root = this.root) {
+        if (root === null) {
+            root = new Node(data);
+            if (this.root === null) {
+                this.root = root;
+            }
+        } else if (data <= root.data) {
+            root.left = this.add(data, root.left)
         } else {
-            const searchTree = function (node) {
-                if (data < node.data) {
-                    if (node.left === null) {
-                        node.left = new Node(data);
-                        return;
-                    } else if (node.left !== null) {
-                        return searchTree(node.left);
-                    }
-                } else if (data > node.data) {
-                    if (node.right === null) {
-                        node.right = new Node(data);
-                        return;
-                    } else if (node.right !== null) {
-                        return searchTree(node.right);
-                    }
-                } else {
-                    return null;
-                }
-            };
-            return searchTree(node);
+            root.right = this.add(data, root.right)
         }
+        return root;
     }
     findMin() {
         let current = this.root;
