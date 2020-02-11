ffb1d9f9cd8ee43f37494a1b184bcfcf95abf936
tarakreddy <lopinti.reddy@ril.com>
2020-02-11 19:48:21 +0530

    refactoring

--- ds_algo/data_structures/stack/stackProblems.js
+++ ds_algo/data_structures/stack/stackProblems.js
@@ -80,3 +80,46 @@ function reversDigits(num) {
 }
 
 //https://www.geeksforgeeks.org/expression-evaluation/
+
+//next greater element in an array
+function printNGE(arr, n) {
+    stack < int > s;
+
+    /* push the first element to stack */
+    s.push(arr[0]);
+
+    // iterate for rest of the elements 
+    for (let i = 1; i < n; i++) {
+
+        if (s.empty()) {
+            s.push(arr[i]);
+            continue;
+        }
+
+        /* if stack is not empty, then 
+           pop an element from stack. 
+           If the popped element is smaller 
+           than next, then 
+        a) print the pair 
+        b) keep popping while elements are 
+        smaller and stack is not empty */
+        while (s.empty() == false && s.top() < arr[i]) {
+            cout << s.top() << " --> " << arr[i] << endl;
+            s.pop();
+        }
+
+        /* push next to stack so that we can find 
+        next greater for it */
+        s.push(arr[i]);
+    }
+
+    /* After iterating over the loop, the remaining 
+    elements in stack do not have the next greater 
+    element, so print -1 for them */
+    while (s.empty() == false) {
+        cout << s.top() << " --> " << -1 << endl;
+        s.pop();
+    }
+}
+
+//
