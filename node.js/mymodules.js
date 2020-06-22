f760511b99ca0df2731a1e6cb68ff4700dc32c5a
tarakreddy <lopinti.reddy@ril.com>
2020-06-22 18:46:55 +0530

    cgs

--- node.js/mymodules.js
+++ node.js/mymodules.js
@@ -2,6 +2,17 @@
 //     return Date();
 // };
 
-var buf = Buffer.from('abc');
+//var buf = Buffer.from('abc');
+//var stream = require('stream');
+//var zlib = require('zlib');
 
-console.log(JSON.stringify(buf));
+
+var http = require('http');
+var url = require('url');
+console.log(url.resolve('', 'https://www.w3schools.com/nodejs/ref_url.asp', 10));
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     var q = url.parse(req.url, true);
+//     res.write(q.href);
+//     res.end();
+// }).listen(8080);
