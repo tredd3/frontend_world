9afb04ff4adafc6cbb7e40312bb6e033744c397f
tredd3 <tarak619reddy@gmail.com>
2020-11-23 22:17:53 +0530

    express + socket.io

--- projects/express_crash_course/index.js
+++ projects/express_crash_course/index.js
@@ -3,8 +3,14 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const logger = require('./middleware/logger');
 const members = require('./Members');
-const router =require('./routes/api/members');
- 
+const router = require('./routes/api/members');
+//using express with socket.io - below is the procedure to share the https server
+//var http = require('http').Server(app)
+//var io=require('socket.io)(http)
+
+//on the client side we do var socket=io() which then it tries to make a connection to the socket io server
+//running on the same host on which the current website is hosted 
+
 const app = express();
 
 // Init middleware
@@ -35,3 +41,12 @@ app.use('/api/members', router);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+//usersList=[]
+// io.on('connection',(socket)=>{
+// usersList.push(socket)
+//   console.log('user connected')
+// })
+
+//http.listen instead of app.listen so that both socket.io and express can run together by using the same node http server 
+//if use app.listen then only express will run and u need to create a separate http server for socket.io
