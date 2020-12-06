9f719dd910a2a766adb0494a3bb86b1795bddfae
tredd3 <tarak619reddy@gmail.com>
2020-12-06 17:51:35 +0530

    mern stack project template

---
+++ projects/mern_stack_project/src/index.js
@@ -0,0 +1,3 @@
+//import 'react' 
+//the above line will look for react module in the node core modules or node_modules folder installed by npm
+//import './react' - this will look for react module in the same working directory
