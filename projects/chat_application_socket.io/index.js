ed2428626fc025d4e364c2797f6febafa9e9d9cc
tredd3 <tarak619reddy@gmail.com>
2021-07-18 19:41:42 +0530

    socket io app

---
+++ projects/chat_application_socket.io/index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+//using express with socket.io - below is the procedure to share the https server
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+// OR
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const usersList = [];
+
+io.of("/").adapter.on("create-room", (room) => {
+  console.log(`room ${room} was created`);
+});
+
+//adapter is the incharege of a given room and is responsible for brodcasting events to all the clients in the room
+io.of("/").adapter.on("join-room", (room, id) => {
+  console.log(`socket ${id} has joined room ${room}`);
+});
+
+io.on("connection", (socket) => {
+  usersList.push(socket);
+  // console.log("user connected");
+  // console.log(socket.id);
+  socket.join("tarak");
+  // when the client emits 'new message', this listens and executes
+  socket.on("chat message", (msg) => {
+    console.log(msg);
+
+    //In order to send an event to everyone, Socket.IO gives us the io.emit() method.
+    io.emit("chat message", { message: msg });
+
+    // If you want to send a message to everyone except for a certain emitting socket
+    socket.broadcast.emit("chat message", {
+      username: socket.username,
+      message: msg,
+    });
+  });
+
+  socket.on("update item", (arg1, arg2, callback) => {
+    console.log(arg1); // 1
+    console.log(arg2); // { name: "updated" }
+    callback({
+      status: "ok",
+    });
+  });
+
+  socket.on("ping", (count) => {
+    console.log(count);
+  });
+
+  // when the user disconnects.. perform this
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    //if you refresh a tab several times you can see it in action.
+    // echo globally that this client has left
+    socket.broadcast.emit("user left", {
+      username: socket.username,
+    });
+  });
+});
--- projects/express_crash_course/index.js
+++ projects/express_crash_course/index.js
@@ -1,52 +1,38 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const logger = require('./middleware/logger');
-const members = require('./Members');
-const router = require('./routes/api/members');
-//using express with socket.io - below is the procedure to share the https server
-//var http = require('http').Server(app)
-//var io=require('socket.io)(http)
-
-//on the client side we do var socket=io() which then it tries to make a connection to the socket io server
-//running on the same host on which the current website is hosted 
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const logger = require("./middleware/logger");
+const members = require("./Members");
+const router = require("./routes/api/members");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+//http.listen instead of app.listen so that both socket.io and express can run together by using the same node http server
+//if use app.listen then only express will run and u need to create a separate http server for socket.io
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Init middleware
 app.use(logger);
 
 // Handlebars Middleware
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Homepage Route
-app.get('/', (req, res) =>
-  res.render('index', {
-    title: 'Member App',
-    members
+// Homepage Route - Server Side Rendering
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member App",
+    members,
   })
 );
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Set static folder - acts as web server
+app.use(express.static(path.join(__dirname, "public")));
 
-// Members API Routes
-app.use('/api/members', router);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-//usersList=[]
-// io.on('connection',(socket)=>{
-// usersList.push(socket)
-//   console.log('user connected')
-// })
-
-//http.listen instead of app.listen so that both socket.io and express can run together by using the same node http server 
-//if use app.listen then only express will run and u need to create a separate http server for socket.io
+// Members API Routes - acts as API server
+app.use("/api/members", router);
